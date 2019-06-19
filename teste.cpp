5de9c8ff28d5ff23d0c8b602dac5e4b6d086b3a8
Matheus Henrique <matherique@gmail.com>
2019-06-19 20:33:48 -0300

    string formating to insert to binary file

--- teste.cpp
+++ teste.cpp
@@ -9,27 +9,30 @@ struct cliente {
   int dia;
   int hora;
 };
+char n[120];
 
-int main(void) {
+
+void cadastrar (string nome) {
   fstream file("teste2.t", ios_base::out | ios_base::binary | ios_base::app);
-  string nome = "matheus henrique dos santos 2212";
-  char n[120];
-
   strcpy(n, nome.c_str());
   file.write(reinterpret_cast<char*>(&n), sizeof(n));
   file.close();
+}
 
+void listar(struct cliente lista[]) {
   fstream file2("teste2.t", ios_base::in | ios_base::binary );
   file2.seekg(0, ios_base::end);
+
   long TAMANHO = file2.tellg();
   long NR_REGS = TAMANHO / sizeof(n);
 
-  cliente lista[NR_REGS];
-
   for (int i = 0; i < NR_REGS; i++) {
+    string nome;
     file2.seekg(i *  sizeof(n), ios_base::beg);
     file2.read(reinterpret_cast<char*>(&n), sizeof(n));
+
     nome.assign(n);
+
     string n = nome.substr(0, nome.length() - 4);
     int d = stoi(nome.substr(nome.length() - 4, 2));
     int h = stoi(nome.substr(nome.length() - 2));
@@ -37,6 +40,46 @@ int main(void) {
     lista[i].dia = d;
     lista[i].hora = h;
   }
+  file2.close();
+}
+
+int getSize(void) {
+  fstream file2("teste2.t", ios_base::in | ios_base::binary );
+  file2.seekg(0, ios_base::end);
+  long TAMANHO = file2.tellg();
+  long NR_REGS = TAMANHO / sizeof(n);
+  file2.close();
+  return NR_REGS;
+}
+
+string addFillZero(int number) {
+  string n = to_string(number);
+  if (n.length() == 1) {
+      return "0" + n;
+  }
+  return n;
+}
+
+string  convertStructToString( cliente dados) {
+  string ret, nome, dia, hora;
+  nome = dados.nome;
+  dia = addFillZero(dados.dia);
+  hora = addFillZero(dados.hora);
+
+  return nome + dia + hora;
+}
+
+int main(void) {
+  cliente teste;
+
+  teste.nome = "camila";
+  teste.dia = 1;
+  teste.hora = 8;
+  string result = convertStructToString(teste);
+  cadastrar(result);
+  int n = getSize();
+  cliente lista[n];
+  listar(lista);
 
   long qtd = sizeof(lista) / (32 + 4 + 4);
   for (int i = 0; i < qtd; i++) {
@@ -45,8 +88,7 @@ int main(void) {
     cout << "Hora: " << lista[i].hora << endl;
     cout << "=====================================================" << endl;
   }
-
-
-  file2.close();
   return 0;
 }
+
+
